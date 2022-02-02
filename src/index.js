8252875e4203b996d5a3833e2f1bbf0acf0f7784
Shirley-Aguilar-Aguilar <shirley.aguilar.agui@gmail.com>
2022-02-02 01:47:21 -0500

    creando secciones  en html y  función de evento para cambio de página en js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+
+
+ 
+
+//cambio de página 1 a 2
+function eventSecondPage(){
+   
+    document.getElementById("firstPage").style.display= "none"; //ocultar página
+    document.getElementById("secondPage").style.display= "block";//mostrar página
+    
+}
+document.addEventListener("click", eventSecondPage); //registrar evento onclick
+
+
+
