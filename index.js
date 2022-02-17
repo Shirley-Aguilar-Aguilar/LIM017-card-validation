5399351674bc66a99c447e948149c8f8d775daea
Shirley-Aguilar-Aguilar <shirley.aguilar.agui@gmail.com>
2022-02-17 12:48:29 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,105 @@
+import validator from './validator.js';
+//cambio de página 1 a 2
+function eventSecondPage(){
+    document.getElementById("firstPage").style.display= "none"; //ocultar página
+    document.getElementById("secondPage").style.display= "block";//mostrar página  
+}
+
+let buttonInput= document.getElementById("buttonInput");
+buttonInput.addEventListener("click", eventSecondPage); //registrar evento click
+
+//validar que número de tarjeta sea solo número
+function onlyNumbers(input){
+   //este regex valida que solo existan numeros
+   var regexOnlyNumbers =  new RegExp('^[0-9]+$');
+    
+   return regexOnlyNumbers.test(input);
+}
+
+function manageOnlyNumbersValidation() {  
+   let creditCardNumberValue= document.getElementById("creditCardNumber").value; 
+   console.log("que cosa capturamos: " + creditCardNumberValue);
+   //si el numero de tarjeta no hace match con el regex de solo numeros
+   if (creditCardNumberValue !== '') {
+      if(!onlyNumbers(creditCardNumberValue)) {
+         document.getElementById("dataNoValid").style.display= "block";
+      }
+   }
+}
+
+let creditCardNumber= document.getElementById("creditCardNumber");
+creditCardNumber.addEventListener("input", manageOnlyNumbersValidation);
+
+      
+ //datos  obstenidos del usuario
+function inputNumber(){   
+   document.getElementById("dataNoValid").style.display= "none";
+   let creditCardNumber= parseInt(document.getElementById("creditCardNumber").value) ;
+   console.log( creditCardNumber);
+   
+   validator.isValid(creditCardNumber);
+   validator.maskify(creditCardNumber);
+   validator.getIssuer(creditCardNumber);
+
+
+   // dato que retorna con digitos ocultos
+   let resultFinalOfMaskify = validator.maskify(creditCardNumber);
+   document.getElementById("creditCardNumber").value= resultFinalOfMaskify ;
+
+   //dato que retorna si la tarjeta es valida o no
+   let resultFinalOfIsValid = validator.isValid(creditCardNumber);
+   if (resultFinalOfIsValid) {
+        document.getElementById("cardValid").style.display= "block";
+   } else {
+        document.getElementById("cardNotValid").style.display= "block"; 
+   }
+
+    //resultado de get issuer
+   let resultFinalOfIssuer = validator.getIssuer(creditCardNumber);
+   if(resultFinalOfIsValid){
+      if (resultFinalOfIssuer == "americanExpress"){
+         document.getElementById("americanExpress").style.display= "block";
+         document.getElementById("visa").style.display= "none";
+         document.getElementById("masterCard").style.display= "none";
+         document.getElementById("invalid").style.display= "none";
+      }
+      else if (resultFinalOfIssuer == "visa"){
+         document.getElementById("visa").style.display= "block";
+         document.getElementById("americanExpress").style.display= "none";
+         document.getElementById("masterCard").style.display= "none";
+         document.getElementById("invalid").style.display= "none";
+      }
+      else if (resultFinalOfIssuer == "mastercard"){
+         document.getElementById("masterCard").style.display= "block";
+         document.getElementById("visa").style.display= "none";
+         document.getElementById("americanExpress").style.display= "none";
+         document.getElementById("invalid").style.display= "none";
+      }
+      else  if (resultFinalOfIssuer == "invalid") { 
+         document.getElementById("masterCard").style.display= "none";
+         document.getElementById("visa").style.display= "none";
+         document.getElementById("americanExpress").style.display= "none";
+         document.getElementById("invalid").style.display= "block";
+      }
+
+   } 
+}
+
+let buttonNumber= document.getElementById("buttonNumber");
+buttonNumber.addEventListener("click",inputNumber) ;
+
+//limpiar datos
+function clearData(){
+   document.getElementById("formu").reset();
+   document.getElementById("cardValid").style.display= "none";
+   document.getElementById("cardNotValid").style.display= "none";
+   document.getElementById("dataNoValid").style.display= "none";  
+   document.getElementById("visa").style.display= "none";
+   document.getElementById("americanExpress").style.display= "none";
+   document.getElementById("masterCard").style.display= "none";
+   document.getElementById("invalid").style.display= "none";
+}
+
+let buttonClear= document.getElementById("buttonClear");
+buttonClear.addEventListener("click",clearData) ;
+
---
+++ validator.js
@@ -0,0 +1,133 @@
+const validator = {
+
+  // ALGORITMO DE LUHN
+
+  isValid: function (creditCardNumber) {
+    // pasar número en array en orden inverso
+    function reverseNumber(input) {
+      let inputAsString = input.toString();
+      let result = "";
+
+      for (let i = inputAsString.length - 1; i >= 0; i--) {
+        var digitoAuxiliar = inputAsString[i];
+        result = result + digitoAuxiliar;
+        console.log(result);
+      }
+      return parseInt(result);
+    }
+    // multiplicar x2 los numeros en posiciones pares  y se sumen los digitos de los números mayores a 10 
+    function applyLogicForEvenNumbers(input) {
+      let inputAsString = input.toString();
+      let result = "";
+
+      var outputAuxCifra = 0;
+      for (var i = 0; i <= inputAsString.length - 1; i++) {
+        var digitoAuxiliar = parseInt(inputAsString[i]);
+        outputAuxCifra = digitoAuxiliar;
+
+        //encontrar los números en las posiciones pares
+        if (i % 2 !== 0) {
+          console.log("inputAsString[i] tiene posición par y  su valor es: " + digitoAuxiliar);
+          var doubleNumber = digitoAuxiliar * 2; // multiplicar los pares
+          console.log("digitouxiliar multiplicado *2 es: " + doubleNumber);
+
+          //sumar los digitos multiplicados mayores a 10
+          if (doubleNumber >= 10) {
+            outputAuxCifra = 1 + (doubleNumber % 10);
+          } else {
+            outputAuxCifra = doubleNumber;
+          }
+        }
+
+        result = result + outputAuxCifra.toString();
+      }
+      return parseInt(result);
+    }
+
+    //suma total multiplo de 10
+    function getSummOfNumbers(number) {
+      let inputAsString = number.toString();
+      let sumatoria = 0;
+      for (var i = 0; i <= inputAsString.length - 1; i++) {
+        sumatoria = sumatoria + parseInt(inputAsString[i]);
+      }
+      return sumatoria;
+    }
+
+    //llamando funciones para capturar el resultado de las funciones
+    var reversedNumber = reverseNumber(creditCardNumber);
+    var encodedNumber = applyLogicForEvenNumbers(reversedNumber);
+    var sumatoriaCifras = getSummOfNumbers(encodedNumber);
+    console.log("la sumatoria de cifras es: " + sumatoriaCifras);
+
+    //validar suma total
+    var finalResult = sumatoriaCifras % 10 === 0;
+    console.log(finalResult);
+
+    return finalResult;
+    
+  },
+  // OCULTAR TODOS LOS INDICES MENOS LOS 4 ÚTIMOS .
+
+  maskify: function (creditCardNumber) {
+    function maskify(input) {
+      let inputAsString = input.toString();
+      const SIZE_OF_VISIBLE_NUMBERS = 4;
+
+      if (inputAsString.length <= SIZE_OF_VISIBLE_NUMBERS) {
+        //retorna el valor sin ningun cambio
+        return inputAsString;
+      } else {
+        let maskifyInput = "";
+
+        for (let i = 0; i <= inputAsString.length - 1; i++) {
+          if (inputAsString.length - i <= SIZE_OF_VISIBLE_NUMBERS) {
+            maskifyInput = maskifyInput + inputAsString[i];
+          } else {
+            maskifyInput = maskifyInput + "#";
+          }
+        }
+        return maskifyInput;
+      }
+    }
+    return maskify(creditCardNumber);
+  },
+
+  getIssuer: function(creditCardNumber){
+    function cardType(){  
+      const VALID_NUMBER_AMERICAN_EXPRESS = 3 ;
+      const VALID_NUMBER_VISA = 4 ;
+      const VALID_NUMBER_MASTERCARD = 5 ;
+
+      
+      // obtener el 1 numero de la tarjeta y la cantidad de numeros
+      let firstNumberFromCard = parseInt(creditCardNumber.toString()[0]);
+      console.log("El primer digito es : " + firstNumberFromCard);
+      let digitQuantity = parseInt(creditCardNumber.toString().length);
+      console.log("La cantidad de digitos totales es : "+  digitQuantity);
+      let result= "invalid";
+      if ((digitQuantity === 15) && (firstNumberFromCard === VALID_NUMBER_AMERICAN_EXPRESS)) {
+       result= "americanExpress";
+     
+      } else if ((digitQuantity === 16)) {
+          if (firstNumberFromCard === VALID_NUMBER_VISA){
+             result = "visa";
+          }
+           else if(firstNumberFromCard === VALID_NUMBER_MASTERCARD) {
+              result = "mastercard";
+            }
+           
+        }
+        console.log(result);
+        return result ;
+    }
+     return cardType(creditCardNumber);
+   
+  },
+
+};
+
+export default validator;
+
+
+ 
